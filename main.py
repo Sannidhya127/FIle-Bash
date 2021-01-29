1ab9acd8479c22c242ff3bdb82c12dd6deeb8924
Sannidhya Dasgupta <shubhradasgupta8@gmail.com>
2021-01-30 00:08:40 +0530

    Set the colors but was facing some wierd issues that I have never seen before. Its midnight now, so just commented out the code and made it like before. Have plans to look into it properly after the annual exams

--- main.py
+++ main.py
@@ -369,7 +369,7 @@ def BashApi():
 
 
 if __name__ == '__main__':
-    color = False
+    # color = False
     color_red = False
     color_yellow = False
     color_violet = False
@@ -419,29 +419,9 @@ if __name__ == '__main__':
 
     while True:
         d = os.getcwd()
-        if color == False:
-            comd = input(f"{fg('green_1')}{d}: {attr('reset')}")
-        elif color_red == True:
-            comd = input(f"{fg('red_1')}{d}: {attr('reset')}")
-        elif color_violet == True:
-            comd = input(f"{fg('violet')}{d}: {attr('reset')}")
-        elif color_olive == True:
-            comd = input(f"{fg('dark_olive_green_1a')}{d}: {attr('reset')}")
-        elif color_magenta == True:
-            comd = input(f"{fg('magenta_1')}{d}: {attr('reset')}")
-        elif color_pink == True:
-            comd = input(f"{fg('red_1')}{d}: {attr('reset')}")
-        elif color_blue == True:
-            comd = input(f"{fg('red_1')}{d}: {attr('reset')}")
-        elif color_white == True:
-            comd = input(f"{fg('red_1')}{d}: {attr('reset')}")
-        elif color_yellow == True:
-            comd = input(f"{fg('red_1')}{d}: {attr('reset')}")
-        elif color_black == True:
-            comd = input(f"{fg('red_1')}{d}: {attr('reset')}")
-        elif color_invisible == True:
-            comd = input(f"{fg('red_1')}{d}: {attr('reset')}")
-        elif comd == "bash --help":
+        # if color == False:
+        comd = input(f"{fg('green_1')}{d}: {attr('reset')}")
+        if comd == "bash --help":
             print(f"ls (list all files and directories)\n\nls --docs (list all test files)\n\nls --imgs (list all image files)\n\nls --aud (list all audio files)\n\nls --med(list all video files)\n\nls --progs (lists all program files)\n\ndelf filename (deletes a file)\n\ndeld foldername (deletes a folder)\n\nmv fileOrFolderName (renames a file or folder)\n\ncrf filename (creates a new file or directory)\n\ncrd foldername (this creates a directory)\n\ncd (prints the current working directory)\n\ncd --to (changes the current working directory)\n\nls --check (checks a given path for existence)\n\ncomp file1 file2 (compares the text of file2 with file1 and reports the differences)\n\nbash --q (quits file bash)\n\nFor More Queries Email us at filebash45@gmail.com")
         elif comd == "ls":
             AllFiles()
@@ -486,8 +466,24 @@ if __name__ == '__main__':
         elif comd == "rm -rf":
             subprocess.run(comd)
             # cng --name "Snan"
-        elif comd == "cng --color red":
-            color_red = True
+        # elif comd == "cng --color red":
+        #     color_red = True
+        # elif comd == "cng --color black":
+        #     color_black = True
+        # elif comd == "cng --color blue":
+        #     color_blue = True
+        # elif comd == "cng --color none":
+        #     color_invisible = True
+        # elif comd == "cng --color magenta":
+        #     color_magenta = True
+        # elif comd == "cng --color pink":
+        #     color_pink = True
+        # elif comd == "cng --color violet":
+        #     color_violet = True
+        # elif comd == "cng --color white":
+        #     color_white = True
+        # elif comd == "cng --color yellow":
+        #     color_yellow = True
 
         elif comd[0:3] == "pip":
             subprocess.run(comd)
@@ -497,6 +493,26 @@ if __name__ == '__main__':
             readFile(comd)
         elif comd == "":
             pass
+        # elif color_red == True:
+        #     comd = input(f"{fg('red_1')}{d}: {attr('reset')}")
+        # elif color_violet == True:
+        #     comd = input(f"{fg('violet')}{d}: {attr('reset')}")
+        # elif color_olive == True:
+        #     comd = input(f"{fg('dark_olive_green_1a')}{d}: {attr('reset')}")
+        # elif color_magenta == True:
+        #     comd = input(f"{fg('magenta_1')}{d}: {attr('reset')}")
+        # elif color_pink == True:
+        #     comd = input(f"{fg('pink_1')}{d}: {attr('reset')}")
+        # elif color_blue == True:
+        #     comd = input(f"{fg('blue')}{d}: {attr('reset')}")
+        # elif color_white == True:
+        #     comd = input(f"{fg('default')}{d}: {attr('reset')}")
+        # elif color_yellow == True:
+        #     comd = input(f"{fg('yellow_1')}{d}: {attr('reset')}")
+        # elif color_black == True:
+        #     comd = input(f"{fg('grey_3')}{bg('white')}{d}: {attr('reset')}")
+        # elif color_invisible == True:
+        #     comd = input(f"{fg('grey_3')}{d}: {attr('reset')}")
         elif comd[0:3] == "git":
             subprocess.run(comd)
         elif comd[0:6] == "python":
